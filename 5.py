09092a6646d10a96a0a2b1da9c3c428e8ef42bcc
margulanz <37156990+margulanz@users.noreply.github.com>
2023-02-14 10:29:51 +0600

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,54 +1,43 @@
 class Version():
-  def __init__(self,s):
-    self.version = self.convert(s if s[-1] == '.' else s+'.')
-  def convert(self,s):
-    tmp = ''
-    ls = []
-    for i in range(len(s)):
-      if s[i] == '.':
-        ls.append(int(tmp))
-        tmp = ''
-        continue
-      tmp+=s[i]
-    return ls
-  def __eq__(self, obj):
-    return self.version == obj.version
-  def __le__(self,obj):
-    l = min(len(self.version),len(obj.version))
-    for i in range(l):
-      if self.version[i]>obj.version[i]:
-        return False
-      
-    return True
-  def __ge__(self,obj):
-    l = min(len(self.version),len(obj.version))
-    for i in range(l):
-      if self.version[i]<obj.version[i]:
-        return False
-      
-    return True
-  def __lt__(self,obj):
-    l = min(len(self.version),len(obj.version))
-    for i in range(l):
-      if self.version[i]>obj.version[i]:
-        return False
-    if obj == self:
-      return False
-    return True
-  def __gt__(self,obj):
-    l = min(len(self.version),len(obj.version))
-    for i in range(l):
-      if self.version[i]<obj.version[i]:
-        return False
-    if self == obj:
-      return False
-    return True
-    
-'''
-10.1 A
-10.1.21 B
+    def __init__(self,major = 0,minor = 0,patch = 0):
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+    def __lt__(self,obj):
+        if self.major>obj.major:
+            return False
+        else:
+            if self.minor>obj.minor:
+                return False
+            else:
+                if self.patch>obj.patch:
+                    return False
+                else:
+                    return True
+    def __gt__(self,obj):
+        if self.major<obj.major:
+            return False
+        else:
+            if self.minor<obj.minor:
+                return False
+            else:
+                if self.patch<obj.patch:
+                    return False
+                else:
+                    return True
+    def __eq__(self,obj):
+        return self.major == obj.major and self.minor == obj.minor and self.patch == obj.patch
 
-'''
-A = Version('11.1.1')
-B = Version('11.2')
-print(A < B)
+
+def solution(A,B):
+    a = Version(*A)
+    b = Version(*B)
+    if a == b:
+        return 0
+    elif a<b:
+        return -1
+    else:
+        return 1
+
+# Example
+print(solution([1,2],[1,2,4]))
